f2450a4063984d46d8135297a771cfd24358bfa8
Matt Rayner <m@rayner.io>
2018-01-03 17:51:16 +0000

    Added named subject as per feedback

--- spec/ralyxa/configuration_spec.rb
+++ spec/ralyxa/configuration_spec.rb
@@ -1,17 +1,17 @@
 require 'ralyxa/configuration'
 
 RSpec.describe Ralyxa::Configuration do
-  subject { Ralyxa::Configuration.new }
+  subject(:ralyxa_configuration) { Ralyxa::Configuration.new }
 
   describe '#initialize' do
     it 'sets default configuration values' do
-      expect(subject.validate_requests).to eq(true)
+      expect(ralyxa_configuration.validate_requests).to eq(true)
     end
   end
 
   describe '#validate_requests?' do
     it 'returns the value of validate_request' do
-      configuration = subject
+      configuration = ralyxa_configuration
       expect(configuration.validate_requests?).to eq(true)
 
       configuration.validate_requests = false
